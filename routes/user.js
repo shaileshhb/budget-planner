046ded9f0fffe2023a04983fe49ca1c407001403
shaileshhb <shaileshb.0720@gmail.com>
2022-09-18 13:03:24 +0530

    test push

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,10 @@ const router = express.Router()
 
 const { registerUser, login } = require('../controllers/authentication')
 
+router.get("/", (req, res) => {
+  console.log(" at api/v1 get request");
+})
+
 router.post('/register', registerUser)
 router.post('/login', login)
 
