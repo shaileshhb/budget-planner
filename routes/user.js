30f1c51f8e49c3d0dcd9a669bb1adb8a60283a93
shaileshhb <shaileshb.0720@gmail.com>
2022-09-18 19:51:17 +0530

    test post route changed

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,12 @@ const router = express.Router()
 
 const { registerUser, login } = require('../controllers/authentication')
 
-router.post("/", (req, res) => {
+// router.post("/", (req, res) => {
+//   console.log(" at api/v1 get request");
+//   res.status(200).send("<h1>Test...</h1>")
+// })
+
+router.route("/test").post((req, res) => {
   console.log(" at api/v1 get request");
   res.status(200).send("<h1>Test...</h1>")
 })
