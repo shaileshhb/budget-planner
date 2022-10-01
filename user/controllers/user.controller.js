47d66d05cbcde77a81abb0f2ea239a875224381c
shaileshhb <shaileshb.0720@gmail.com>
2022-10-01 19:54:32 +0530

    update user added

--- user/controllers/user.controller.js
+++ user/controllers/user.controller.js
@@ -1,6 +1,34 @@
 const db = require("../../models/index")
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../../errors');
+const { Op } = require("sequelize")
+
+const updateUser = async (req, res) => {
+  const transaction = await db.sequelize.transaction();
+
+  try {
+    const user = req.body
+    user.id = req.params.userID
+
+    await validateUser(user)
+
+    await db.user.update(user, {
+      where: {
+        id: user.id
+      },
+      transaction: transaction,
+    })
+
+    res.status(StatusCodes.ACCEPTED).json(null)
+
+  } catch (err) {
+    console.error(err);
+
+    await transaction.rollback()
+    throw new CustomError.BadRequestError(err)
+  }
+
+}
 
 const getUser = async (req, res) => {
   const transaction = await db.sequelize.transaction();
@@ -29,6 +57,38 @@ const getUser = async (req, res) => {
 
 }
 
+const validateUser = async (user) => {
+
+  if (!user.email) {
+    throw new CustomError.BadRequestError("Email must be specified")
+  }
+
+  // check if email exist
+  const findEmail = await db.user.findOne({
+    where: {
+      id: { [Op.ne]: user.id },
+      email: user.email
+    }
+  })
+
+  if (findEmail) {
+    throw new CustomError.BadRequestError("Email already exist")
+  }
+
+  // check if username exist
+  const findUsername = await db.user.findOne({
+    where: {
+      id: { [Op.ne]: user.id },
+      username: user.username
+    }
+  })
+
+  if (findUsername) {
+    throw new CustomError.BadRequestError("Username already exist")
+  }
+}
+
 module.exports = {
   getUser,
+  updateUser,
 }
